fde1fb6b87dcd683c333ef9787af5cc822da65c4
Nitish Kumar Jha <nitjha1@publicisgroupe.net>
2019-06-14 16:58:37 +0530

    Styled Components

--- src/userInfo.js
+++ src/userInfo.js
@@ -1,12 +1,40 @@
 import React from "react";
+import styled from 'styled-components';
+
+const UserInfoDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 20px;
+  background: #fff;
+  padding: 2rem;
+  border-radius: 40px;
+  font-size: 1.2rem;
+  box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.12);
+  margin-bottom: 40px;
+`;
+
+const Avatar = styled.div`
+    border-right: 1px solid rgba(0, 0, 0, 0.12);
+    display: flex;
+    align-items: flex-start;
+`;
+
+const Content = styled.div`
+    line-height: 2rem;
+`;
+
+const Image = styled.img`
+    border-radius: 50%;
+    border: 2px solid #141414;
+`;
 
 const UserInfo = props => {
     return (
-        <div className="user-info">
-            <div className="avatar">
-                <img src={props.user.avatar_url} width="250px" alt="User"/>
-            </div>
-            <div className="content">
+        <UserInfoDiv>
+            <Avatar >
+                <Image src={props.user.avatar_url} width="250px" alt="User"/>
+            </Avatar>
+            <Content>
                 <h1>{ props.user.name }</h1>
                 <p>
                     <strong>Bio: </strong>
@@ -24,8 +52,8 @@ const UserInfo = props => {
                     <strong>Following:</strong>
                     {props.user.following}
                 </p>
-            </div>
-        </div>
+            </Content>
+        </UserInfoDiv>
     );
 };
 
